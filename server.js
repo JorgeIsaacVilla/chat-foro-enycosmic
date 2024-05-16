08c00389594b05cfb221906b86f022931e4c0cbd
JorgeIsaacVilla <jvilla10@outlook.es>
2024-05-16 16:22:25 -0500

    Actualizacion de servidor para servir archivos estaticos

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Servir los archivos estáticos de la carpeta "public"
-app.use(express.static('public'));
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// Ruta de inicio
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('Un usuario se ha conectado.');
@@ -20,9 +27,12 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Un usuario se ha desconectado.');
     });
+
+    // Enviar un evento al cliente cuando el servidor esté listo
+    socket.emit('server ready', `Servidor inicializado correctamente en el puerto ${PORT}`);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Definir solo el número del puerto
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}/`);
 });
